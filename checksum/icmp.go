d771d6b9e0447d7ffa34052e79b504ad0f6b3a7c
xgfone <xgfone@126.com>
2015-12-19 00:36:14 +0800

    Add the package of `checksum`.

---
+++ checksum/icmp.go
@@ -0,0 +1,20 @@
+package checksum
+
+func ICMP(data []byte) uint16 {
+	var (
+		sum    uint32
+		length int = len(data)
+		index  int
+	)
+	for length > 1 {
+		sum += uint32(data[index])<<8 + uint32(data[index+1])
+		index += 2
+		length -= 2
+	}
+	if length > 0 {
+		sum += uint32(data[index])
+	}
+	sum += (sum >> 16)
+
+	return uint16(^sum)
+}
