603689651d8bc3cc37b0fef040847c42b8fcb6ab
xgfone <xgfone@126.com>
2015-12-19 00:55:12 +0800

    Add the convenient functions formating time.

--- datetime/time.go
+++ datetime/time.go
@@ -6,9 +6,22 @@ import (
 
 const (
 	FMT_DATE     = "2006-01-02"
-	FMT_DATETIME = "2006-01-02 15-04-05"
+	FMT_DATETIME = "2006-01-02 15:04:05"
+	FMT_TIME     = "15:04:05"
 )
 
 func Now() int64 {
 	return time.Now().Unix()
+}
+
+func ToTime(t time.Time) string {
+	return t.Format(FMT_TIME)
+}
+
+func ToDate(t time.Time) string {
+	return t.Format(FMT_DATE)
+}
+
+func ToDateTime(t time.Time) string {
+	return t.Format(FMT_DATETIME)
 }
