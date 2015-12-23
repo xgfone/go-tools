fe282a421117c087cb32f75079dbedd539d5ea55
xgfone <xgfone@126.com>
2015-12-23 23:24:24 +0800

    Update the server.

--- server/server.go
+++ server/server.go
@@ -1,36 +1,102 @@
 package server
 
 import (
+	"errors"
 	"fmt"
 	"net"
 )
 
-type App interface {
-	Handle(*net.TCPConn)
-}
+type Handle func(*net.TCPConn)
 
-func WrapError(app App, conn *net.TCPConn) {
+func WrapError(handle Handle, conn *net.TCPConn) {
 	defer func() {
 		if err := recover(); err != nil {
 			fmt.Printf("Get a error: %v\n", err)
 		}
 	}()
-	app.Handle(conn)
+	handle(conn)
+	conn.Close()
 }
 
-func TCPServerForever(net, addr string, app App) (e error) {
-	ln, err := net.Listen(net, addr)
+//	// TCP Server
+// package main
+//
+// import (
+// 	"fmt"
+// 	"github.com/xgfone/go-tools/server"
+// 	"io"
+// 	"net"
+// )
+//
+// func handle(conn *net.TCPConn) {
+// 	buf := make([]byte, 1024)
+// 	for {
+// 		n, err := conn.Read(buf)
+// 		if err != nil {
+// 			if err == io.EOF {
+// 				fmt.Println("Conn broke off")
+// 				return
+// 			}
+// 			fmt.Println(err)
+// 		} else {
+// 			fmt.Printf("Receive %v bytes: %v\n", n, string(buf[:n]))
+// 		}
+// 	}
+// }
+//
+// func main() {
+// 	err := server.TCPServerForever("tcp", ":8000", handle)
+// 	fmt.Println(err)
+// }
+//
+// // TCP Client
+// package main
+//
+// import (
+// 	"fmt"
+// 	"net"
+// 	"os"
+// 	"time"
+// )
+//
+// func main() {
+// 	conn, err := net.Dial("tcp", "127.0.0.1:8000")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		os.Exit(1)
+// 	}
+// 	tcp, ok := conn.(*net.TCPConn)
+// 	for ok {
+// 		n, err := tcp.Write([]byte("hello"))
+// 		if err != nil {
+// 			fmt.Printf("Error: %v\n", err)
+// 			return
+// 		}
+// 		fmt.Printf("Write %v bytes\n", n)
+// 		time.Sleep(time.Second)
+// 	}
+// }
+
+func TCPServerForever(network, addr string, handle Handle) (e error) {
+	ln, err := net.Listen(network, addr)
 	if err != nil {
 		return err
 	}
+
+	tcpln, ok := ln.(*net.TCPListener)
+	if !ok {
+		return errors.New("Must be a TCP Listener")
+	}
+
+	fmt.Printf("Listen on %v\n", addr)
 	for {
-		conn, err := ln.AcceptTCP()
+		conn, err := tcpln.AcceptTCP()
 		if err != nil {
 			e = err
 			fmt.Printf("Failed to AcceptTCP: %v\n", err)
 		} else {
 			e = nil
-			go WrapError(app, conn)
+			go WrapError(handle, conn)
 		}
 	}
 	return nil
