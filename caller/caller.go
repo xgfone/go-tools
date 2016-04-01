5d250f1ca82082df9706006187554b2a0880553a
xgfone <xgfone@126.com>
2016-04-01 13:27:07 +0800

    Add the doc of caller

--- caller/caller.go
+++ caller/caller.go
@@ -13,26 +13,31 @@ func getfileno(calldepth int) (string, int) {
 	return "???", -1
 }
 
+// Get the filepath and the line number where calling it.
 func GetFileNo() (string, int) {
 	return getfileno(1)
 }
 
+// Same as GetFileNo, but return the filepath.
 func GetFile() string {
 	file, _ := getfileno(1)
 	return file
 }
 
+// Same as GetFileNo, but return the line number.
 func GetNo() int {
 	_, line := getfileno(1)
 	return line
 }
 
+// Same as GetFile, but return the filename, not the full path.
 func GetFileName() string {
 	file, _ := getfileno(1)
 	_, filename := path.Split(file)
 	return filename
 }
 
+// Same as GetFileNo, but the filename, not the full path.
 func GetFileNameNo() (string, int) {
 	file, line := getfileno(1)
 	_, filename := path.Split(file)
