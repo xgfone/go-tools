35c06d35433447c7cef3de790def2b4ad3006ab6
xgfone <xgfone@126.com>
2016-07-14 14:14:35 +0800

    Add the example for slice

--- slice/slice_test.go
+++ slice/slice_test.go
@@ -1,28 +1,57 @@
 package slice_test
 
 import (
-    "testing"
+	"fmt"
+	"testing"
 
-    "github.com/xgfone/go-tools/slice"
+	"github.com/xgfone/go-tools/slice"
 )
 
 func TestSlice(t *testing.T) {
-    ss := []int{1,2,3,4,5,6}
-    ok := true
-    out := 0
+	ss := []int{1, 2, 3, 4, 5, 6}
+	ok := true
+	out := 0
 
-    ok = slice.SetValueWithDefault(&out, ss, 2, 100)
-    if !ok || out != 3 {
-        t.Fail()
-    }
+	ok = slice.SetValueWithDefault(&out, ss, 2, 100)
+	if !ok || out != 3 {
+		t.Fail()
+	}
 
-    ok = slice.SetValueWithDefault(&out, ss, 6, 100)
-    if !ok || out != 100 {
-        t.Fail()
-    }
+	ok = slice.SetValueWithDefault(&out, ss, 6, 100)
+	if !ok || out != 100 {
+		t.Fail()
+	}
 
-    ok = slice.SetValue(out, ss, 6)
-    if ok {
-        t.Fail()
-    }
+	ok = slice.SetValue(out, ss, 6)
+	if ok {
+		t.Fail()
+	}
+}
+
+func ExampleSetValue() {
+	ss := []int{1, 2, 3, 4, 5, 6}
+	out := -1
+	ok1 := slice.SetValue(&out, ss, 1)
+	fmt.Println(out, ok1)
+
+	out = -1
+	ok2 := slice.SetValue(&out, ss, 6)
+	fmt.Println(out, ok2)
+	// Output:
+	// 2 true
+	// -1 false
+}
+
+func ExampleSetValueWithDefault() {
+	ss := []int{1, 2, 3, 4, 5, 6}
+	out := 0
+	ok1 := slice.SetValue(&out, ss, 1, -1)
+	fmt.Println(out, ok1)
+
+	out = 0
+	ok2 := slice.SetValue(&out, ss, 6, -1)
+	fmt.Println(out, ok2)
+	// Output:
+	// 2 true
+	// -1 fasle
 }
