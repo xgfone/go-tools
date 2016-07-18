0553db5de864288b2d28accc7275dde55d606c92
xgfone <xgfone@126.com>
2016-07-18 15:51:37 +0800

    Add the function of TokenBucket like HTB in Linux TC

---
+++ tb/tb.go
@@ -0,0 +1,91 @@
+// The Simple Token Bucket like HTB in Linux TC.
+package tb
+
+import "time"
+
+type TB struct {
+	buffer  chan bool
+	sleep   time.Duration
+	started bool
+	stoped  bool
+}
+
+// NewTB creates a new token bucket.
+// The default size of the token bucket is 1024.
+func NewTB(rate uint64) *TB {
+	t := &TB{}
+	return t.SetRate(rate).SetBufSize(1024)
+}
+
+func (t TB) calcSleep(rate uint64) time.Duration {
+	return time.Second / time.Duration(rate)
+	//return time.Duration(uint64(time.Second) / rate)
+}
+
+// Set the size of the token bucket.
+//
+// If the token bucket server has been started, calling this method will panic.
+func (t *TB) SetBufSize(size uint) *TB {
+	if t.started {
+		panic("The token bucket server has been started")
+	}
+	t.buffer = make(chan bool, size)
+	return t
+}
+
+// Set the rate to produce the token. The unit is TB/s.
+//
+// Allow that adjust the rate in running.
+func (t *TB) SetRate(rate uint64) *TB {
+	t.sleep = t.calcSleep(rate)
+	return t
+}
+
+// Get the token from the bucket.
+//
+// This method isn't the returned value. That it returns is indicating that you
+// have got the token.
+//
+// If the token bucket server has not been started, calling this method will panic.
+func (t *TB) Get() {
+	if !t.started {
+		panic("The token bucket server isn't started")
+	}
+	<-t.buffer
+	return
+}
+
+// Start to produce the token and put it to the bucket. You can get the token
+// from the bucket by calling t.Get().
+//
+// If the token bucket server has been started, calling this method will panic.
+func (t *TB) Start() {
+	if t.started {
+		panic("The token bucket server has been started")
+	}
+
+	go t.start()
+	t.started = true
+	t.stoped = false
+}
+
+// Stop the token bucket server. Later you can start it again.
+//
+// If the token bucket server has not been started, calling this method will panic.
+func (t *TB) Stop() {
+	if !t.started {
+		panic("The token bucket server isn't started")
+	}
+	t.stoped = true
+	t.started = false
+
+	// In order to let the for loop ends in t.start().
+	time.Sleep(t.sleep)
+}
+
+func (t *TB) start() {
+	for !t.stoped {
+		t.buffer <- true
+		time.Sleep(t.sleep)
+	}
+}
---
+++ tb/tb_test.go
@@ -0,0 +1,22 @@
+package tb_test
+
+import (
+	"fmt"
+
+	"github.com/xgfone/go-tools/tb"
+)
+
+func ExampleTB() {
+	// Get a token from the bucket per second.
+	_tb := tb.NewTB(1)
+	_tb.Start()
+	go func(_tb *tb.TB) {
+		for {
+			_tb.Get()
+			fmt.Println("Get a token") // You can see that print once per second.
+		}
+	}(_tb)
+
+	for {
+	}
+}
