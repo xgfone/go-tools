0a2258c6ce94b9d31fc412a25b1dcb2bc1ad140b
xgfone <xgfone@126.com>
2016-07-18 13:39:29 +0800

    Use the int to replace *pool.GoPool

--- net/server/tcp.go
+++ net/server/tcp.go
@@ -41,9 +41,10 @@ func TCPWrapError(conn *net.TCPConn, handle interface{}) {
 // Start a TCP server and never return. Return an error if returns.
 //
 // network MUST be "tcp", "tcp4", "tcp6".
-// addr is like "host:port", such as "127.0.0.1:8000", and host or port
-// may be omitted.
-func TCPServerForever(network, addr string, gopool *pool.GoPool, handle interface{}) error {
+// addr is like "host:port", such as "127.0.0.1:8000", and host or port may be omitted.
+// size is the number of the pool. If it's 0, it's infinite.
+// handle is the handler to handle the connection came from the client.
+func TCPServerForever(network, addr string, size int, handle interface{}) error {
 	var ln *net.TCPListener
 	if _addr, err := net.ResolveTCPAddr(network, addr); err != nil {
 		return err
@@ -55,7 +56,8 @@ func TCPServerForever(network, addr string, gopool *pool.GoPool, handle interfac
 
 	defer ln.Close()
 
-	if gopool == nil {
+	var gopool *pool.GoPool
+	if size > 0 {
 		gopool = pool.NewGoPool()
 	}
 
@@ -67,9 +69,12 @@ func TCPServerForever(network, addr string, gopool *pool.GoPool, handle interfac
 			_logger.Error("Failed to AcceptTCP: %v", err)
 		} else {
 			_logger.Debug("Get a connection from %v", conn.RemoteAddr())
-			//go TCPWrapError(conn, handle)
-			if err := gopool.Go(TCPWrapError, conn, handle); err != nil {
-				_logger.Error("Failed to run goroutine: %v", err)
+			if gopool == nil {
+				go TCPWrapError(conn, handle)
+			} else {
+				if err := gopool.Go(TCPWrapError, conn, handle); err != nil {
+					_logger.Error("Failed to run goroutine: %v", err)
+				}
 			}
 		}
 	}
