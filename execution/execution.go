2745f98672c461759ce466a98df4dcba9b1ab0a1
xgfone <xgfone@126.com>
2016-12-11 12:30:16 +0800

    Refactor the execution

--- execution/execution.go
+++ execution/execution.go
@@ -4,57 +4,55 @@ package execution
 import (
 	"errors"
 	"os/exec"
+	"sync"
 	"time"
 )
 
 var ErrArguments = errors.New("The arguments are too few")
 
 type Execution struct {
+	*sync.Mutex
+
 	// 0 stands for executing once only, and no retry if failed.
 	Retry int
 
 	// When retring, the it will be paused for that time.Millisecond.
 	Interval int
+
+	// Whether acquire the lock firstly when executing.
+	IsLock bool
 }
 
-func (e Execution) Execute(args []string) error {
-	if len(args) == 0 {
-		return ErrArguments
-	}
-	name := args[0]
-	args = args[1:]
-
-	sleep := time.Millisecond * time.Duration(e.Interval)
-	retry := e.Retry
-	ok := false
-	var err error
-	for retry >= 0 {
-		if err = exec.Command(name, args...).Run(); err == nil {
-			ok = true
-			break
-		}
-		retry--
-		if retry >= 0 {
-			time.Sleep(sleep)
-		}
-	}
-
-	if ok {
-		return nil
-	} else {
-		return err
-	}
+func (e *Execution) Execute(args []string) error {
+	_, err := e.execute(args, false, func(name string, args []string, eout bool) (string, error) {
+		err := exec.Command(name, args...).Run()
+		return "", err
+	})
+	return err
 }
 
-func (e Execution) Output(args []string) (string, error) {
+func (e *Execution) Output(args []string) (string, error) {
 	return e.output(args, false)
 }
 
-func (e Execution) ErrOutput(args []string) (string, error) {
+func (e *Execution) ErrOutput(args []string) (string, error) {
 	return e.output(args, true)
 }
 
-func (e Execution) output(args []string, eout bool) (string, error) {
+func (e *Execution) output(args []string, eout bool) (string, error) {
+	return e.execute(args, eout, func(name string, args []string, eout bool) (string, error) {
+		var err error
+		var out []byte
+		if eout {
+			out, err = exec.Command(name, args...).CombinedOutput()
+		} else {
+			out, err = exec.Command(name, args...).Output()
+		}
+		return string(out), err
+	})
+}
+
+func (e *Execution) execute(args []string, eout bool, executor func(string, []string, bool) (string, error)) (string, error) {
 	if len(args) == 0 {
 		return "", ErrArguments
 	}
@@ -62,17 +60,24 @@ func (e Execution) output(args []string, eout bool) (string, error) {
 	args = args[1:]
 
 	sleep := time.Millisecond * time.Duration(e.Interval)
+	if sleep < 0 {
+		sleep = 0
+	}
+
 	retry := e.Retry
 	ok := false
 	var err error
-	var out []byte
+	var out string
 
 	for retry >= 0 {
-		if eout {
-			out, err = exec.Command(name, args...).CombinedOutput()
-		} else {
-			out, err = exec.Command(name, args...).Output()
-		}
+		func() {
+			if e.IsLock {
+				e.Lock()
+				defer e.Unlock()
+			}
+			out, err = executor(name, args, eout)
+		}()
+
 		if err == nil {
 			ok = true
 			break
@@ -85,7 +90,7 @@ func (e Execution) output(args []string, eout bool) (string, error) {
 	}
 
 	if ok {
-		return string(out), nil
+		return out, nil
 	} else {
 		return "", err
 	}
--- execution/execution_test.go
+++ execution/execution_test.go
@@ -1,18 +1,46 @@
 package execution_test
 
-import "github.com/xgfone/go-tools/execution"
+import (
+	"fmt"
+
+	"github.com/xgfone/go-tools/execution"
+)
 
 func ExampleExecution_Execute() {
 	e := execution.Execution{Retry: 1, Interval: 1000}
 	err := e.Execute([]string{"ls", "."})
+	if err != nil {
+		fmt.Println("ERROR", err)
+	} else {
+		fmt.Println("OK")
+	}
+
+	// Output:
+	// OK
 }
 
 func ExampleExecution_Output() {
 	e := execution.Execution{Retry: 1, Interval: 1000}
 	out, err := e.Output([]string{"ls", "."})
+	if err != nil {
+		fmt.Println("ERROR")
+	} else {
+		fmt.Println("OK")
+	}
+
+	// Output:
+	// OK
 }
 
 func ExampleExecution_ErrOutput() {
 	e := execution.Execution{Retry: 1, Interval: 1000}
 	out, err := e.ErrOutput([]string{"ls", "."})
+	if err != nil {
+		fmt.Println("ERROR")
+	} else {
+		fmt.Println("OK")
+	}
+
+	// Output:
+	// OK
 }
