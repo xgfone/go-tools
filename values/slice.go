00c0e511f11a6f8af61f48b65654ef78de1eca5c
xgfone <xgfone@126.com>
2016-12-12 15:47:09 +0800

    Add ToSMap for SMap and ToSlice for Slice

--- values/slice.go
+++ values/slice.go
@@ -1,3 +1,14 @@
 package values
 
 type Slice []interface{}
+
+func ToSlice(v interface{}) (Slice, error) {
+	switch v.(type) {
+	case []interface{}:
+		return Slice(v.([]interface{})), nil
+	case Slice:
+		return v.(Slice), nil
+	default:
+		return nil, ErrTypeOrIndex
+	}
+}
--- values/smap.go
+++ values/smap.go
@@ -1,3 +1,14 @@
 package values
 
 type SMap map[string]interface{}
+
+func ToSMap(v interface{}) (SMap, error) {
+	switch v.(type) {
+	case map[string]interface{}:
+		return SMap(v.(map[string]interface{})), nil
+	case SMap:
+		return v.(SMap), nil
+	default:
+		return nil, ErrTypeOrIndex
+	}
+}
