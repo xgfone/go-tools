c8b7201624688b1be27a68c646740cbe5723c01e
xgfone <xgfone@126.com>
2016-12-12 15:55:38 +0800

    ToSMap and ToSlice don't return error

--- values/slice.go
+++ values/slice.go
@@ -2,13 +2,16 @@ package values
 
 type Slice []interface{}
 
-func ToSlice(v interface{}) (Slice, error) {
+// ToSlice converts v to the type of Slice.
+//
+// Return nil if failed.
+func ToSlice(v interface{}) Slice {
 	switch v.(type) {
 	case []interface{}:
-		return Slice(v.([]interface{})), nil
+		return Slice(v.([]interface{}))
 	case Slice:
-		return v.(Slice), nil
+		return v.(Slice)
 	default:
-		return nil, ErrTypeOrIndex
+		return nil
 	}
 }
--- values/smap.go
+++ values/smap.go
@@ -2,13 +2,16 @@ package values
 
 type SMap map[string]interface{}
 
-func ToSMap(v interface{}) (SMap, error) {
+// ToSMap converts v to the type of SMap.
+//
+// Return nil if failed.
+func ToSMap(v interface{}) SMap {
 	switch v.(type) {
 	case map[string]interface{}:
-		return SMap(v.(map[string]interface{})), nil
+		return SMap(v.(map[string]interface{}))
 	case SMap:
-		return v.(SMap), nil
+		return v.(SMap)
 	default:
-		return nil, ErrTypeOrIndex
+		return nil
 	}
 }
