fb98619670a431448f74141ad1c1f92ab42081c5
xgfone <xgfone@126.com>
2016-12-16 18:29:06 +0800

    Add the functions of SplitN

--- str/string_test.go
+++ str/string_test.go
@@ -33,3 +33,21 @@ func ExampleSplitString() {
 	// Output:
 	// [len=4: ab-fg-12-45]
 }
+
+func ExampleSplitN() {
+	s := "   1   2   3   "
+
+	s1 := str.SplitN(s, unicode.IsSpace, -1)
+	fmt.Printf("[len=%v: -%v-%v-%v-]\n", len(s1), s1[0], s1[1], s1[2])
+
+	s2 := str.SplitN(s, unicode.IsSpace, 0)
+	fmt.Printf("[len=%v: -%v-]\n", len(s2), s2[0])
+
+	s3 := str.SplitN(s, unicode.IsSpace, 1)
+	fmt.Printf("[len=%v: -%v-%v-]\n", len(s3), s3[0], s3[1])
+
+	// Output:
+	// [len=3: -1-2-3-]
+	// [len=1: -   1   2   3   -]
+	// [len=2: -1-2   3   -]
+}
--- str/strings.go
+++ str/strings.go
@@ -11,26 +11,52 @@ import (
 //
 // Notice: SplitSpace(s) == Split(s, unicode.IsSpace).
 func SplitSpace(s string) []string {
-	return Split(s, unicode.IsSpace)
+	return SplitSpaceN(s, -1)
 }
 
 // SplitString splits the string of s by sep, but is not the same as
 // strings.Split(), which the rune in sep arbitrary combination. For example,
 // SplitString("abcdefg-12345", "3-edc") == []string{"ab", "fg", "12", "45"}.
 func SplitString(s string, sep string) []string {
-	return Split(s, func(c rune) bool {
+	return SplitStringN(s, sep, -1)
+}
+
+// Split splits the string of s by the filter. Split will pass each rune to the
+// filter to determine whether it is the separator.
+func Split(s string, filter func(c rune) bool) []string {
+	return SplitN(s, filter, -1)
+}
+
+// SplitSpaceN is the same as SplitStringN, but the whitespace.
+func SplitSpaceN(s string, number int) []string {
+	return SplitN(s, unicode.IsSpace, number)
+}
+
+// SplitStringN is the same as SplitN, but the separator is the string of sep.
+func SplitStringN(s string, sep string, number int) []string {
+	return SplitN(s, func(c rune) bool {
 		for _, r := range sep {
 			if r == c {
 				return true
 			}
 		}
 		return false
-	})
+	}, number)
 }
 
-// Split splits the string of s by the filter. Split will pass each rune to the
-// filter to determine whether it is the separator.
-func Split(s string, filter func(c rune) bool) []string {
+// SplitN splits the string of s by the filter. Split will pass each rune to the
+// filter to determine whether it is the separator, but only number times.
+//
+// If number is equal to 0, don't split; greater than 0, only split number times;
+// less than 0, don't limit. If the leading rune is the separator, it doesn't
+// consume the split number.
+//
+// Notice: The result does not have the element of nil.
+func SplitN(s string, filter func(c rune) bool, number int) []string {
+	if number == 0 {
+		return []string{s}
+	}
+
 	for i, c := range s {
 		if !filter(c) {
 			s = s[i:]
@@ -44,17 +70,22 @@ func Split(s string, filter func(c rune) bool) []string {
 
 	results := make([]string, 0)
 	buf := bytes.NewBuffer(nil)
-	is_new := false
-	for _, c := range s {
+	isNew := false
+	for i, c := range s {
 		if filter(c) {
-			is_new = true
+			isNew = true
 			continue
 		}
 
-		if is_new {
+		if isNew {
 			results = append(results, buf.String())
 			buf = bytes.NewBuffer(nil)
-			is_new = false
+			isNew = false
+			number--
+			if number == 0 {
+				buf.WriteString(s[i:])
+				break
+			}
 		}
 
 		buf.WriteRune(c)
