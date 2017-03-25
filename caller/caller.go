3f5faeafabe6fa14eacec536bf7e64fbdb4fd4b1
xgfone <xgfone@126.com>
2017-03-25 16:26:59 +0800

    Update the comment of the package caller

--- caller/caller.go
+++ caller/caller.go
@@ -1,4 +1,4 @@
-// Get the filename and the line number where to call these functions.
+// Package caller returns the filename and the line number where to call these functions.
 package caller
 
 import (
@@ -14,31 +14,31 @@ func getfileno(calldepth int) (string, int) {
 	return "???", -1
 }
 
-// Get the filepath and the line number where calling it.
+// GetFileNo returns the filepath and the line number where calling it.
 func GetFileNo() (string, int) {
 	return getfileno(1)
 }
 
-// Same as GetFileNo, but return the filepath.
+// GetFile is the same as GetFileNo, but return the filepath.
 func GetFile() string {
 	file, _ := getfileno(1)
 	return file
 }
 
-// Same as GetFileNo, but return the line number.
+// GetNo is the same as GetFileNo, but return the line number.
 func GetNo() int {
 	_, line := getfileno(1)
 	return line
 }
 
-// Same as GetFile, but return the filename, not the full path.
+// GetFileName is the same as GetFile, but return the filename, not the full path.
 func GetFileName() string {
 	file, _ := getfileno(1)
 	_, filename := path.Split(file)
 	return filename
 }
 
-// Same as GetFileNo, but the filename, not the full path.
+// GetFileNameNo is the same as GetFileNo, but the filename, not the full path.
 func GetFileNameNo() (string, int) {
 	file, line := getfileno(1)
 	_, filename := path.Split(file)
