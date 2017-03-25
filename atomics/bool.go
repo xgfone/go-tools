244675c1a999cd9b4834d65dd92adf1e8e2a0305
xgfone <xgfone@126.com>
2017-03-25 16:34:16 +0800

    Update the comment of the package atomics

--- atomics/bool.go
+++ atomics/bool.go
@@ -1,35 +1,35 @@
-// Some atomic types, such as Bool.
+// Package atomics supplies some atomic types, such as Bool.
 package atomics
 
 import "sync"
 
-// The bool type, on which the operationsis atomic.
+// Bool is a bool type, on which the operationsis atomic.
 type Bool struct {
 	sync.Mutex
 	v bool
 }
 
-// New a new Bool variable. You can create it by &Bool{} directly.
+// NewBool returns a new Bool variable. You can create it by &Bool{} directly.
 func NewBool() *Bool {
 	return &Bool{}
 }
 
-// Set it to true.
+// SetTrue sets it to true.
 func (b *Bool) SetTrue() {
 	b.Lock()
 	defer b.Unlock()
 	b.v = true
 }
 
-// Set it to false.
+// SetFalse sets it to false.
 func (b *Bool) SetFalse() {
 	b.Lock()
 	defer b.Unlock()
 	b.v = false
 }
 
-// Get its value.
-func (b Bool) Get() bool {
+// Get returns its value.
+func (b *Bool) Get() bool {
 	b.Lock()
 	defer b.Unlock()
 	return b.v
--- atomics/count.go
+++ atomics/count.go
@@ -2,44 +2,44 @@ package atomics
 
 import "sync/atomic"
 
-// The count type, whose operations are atomic.
+// Count is a count type, whose operations are atomic.
 type Count uint64
 
-// NewCount create a new Count. The default is 0.
+// NewCount creates a new Count. The default is 0.
 func NewCount() *Count {
-	var c Count = 0
+	var c Count
 	return &c
 }
 
-// Add 1 to the current value.
+// Add adds 1 to the current value.
 func (c *Count) Add() *Count {
 	return c.AddWith(1)
 }
 
-// Subtract 1 from the current value.
+// Sub subtracts 1 from the current value.
 func (c *Count) Sub() *Count {
 	return c.SubWith(1)
 }
 
-// Add v to the current value.
+// AddWith adds v to the current value.
 func (c *Count) AddWith(v uint64) *Count {
 	atomic.AddUint64((*uint64)(c), v)
 	return c
 }
 
-// Subtract v from the current value.
+// SubWith subtracts v from the current value.
 func (c *Count) SubWith(v uint64) *Count {
 	atomic.AddUint64((*uint64)(c), ^uint64(v-1))
 	return c
 }
 
-// Set the current value to v.
+// Set sets the current value to v.
 func (c *Count) Set(v uint64) *Count {
 	atomic.StoreUint64((*uint64)(c), v)
 	return c
 }
 
-// Get the current value.
+// Get returns the current value.
 func (c *Count) Get() uint64 {
 	return atomic.LoadUint64((*uint64)(c))
 }
