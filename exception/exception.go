dd8ec2e3262465f4b149e2fda12ccbc8c1c7d581
xgfone <xgfone@126.com>
2017-03-25 13:41:08 +0800

    Add the comment of the package exception

--- exception/exception.go
+++ exception/exception.go
@@ -1,4 +1,4 @@
-// Exception handler like "parent.child.sub-child...sub-child".
+// Package exception is a exception handler like "parent.child.sub-child...sub-child".
 //
 // "A.B" is the parent exception is "A.B.C", "A.B.D" and "A.B.C.D", not "A.C".
 //
@@ -10,14 +10,17 @@ import (
 )
 
 var (
+	// SEPARATOR is the separator between the parent and the child exceptions.
 	SEPARATOR = "."
 )
 
+// Exception is the exception handler.
 type Exception struct {
 	_type string
 	msg   string
 }
 
+// New returns a new Exception.
 func New(_type string, msg string) Exception {
 	return Exception{_type: fixType(_type), msg: msg}
 }
@@ -27,41 +30,51 @@ func fixType(_type string) string {
 	return strings.Trim(_type, SEPARATOR)
 }
 
+// Error implements the error interface.
 func (e Exception) Error() string {
 	return e.String()
 }
 
+// String implements the interface fmt.Stringer.
 func (e Exception) String() string {
 	return fmt.Sprintf("Exception<%s>: %s", e._type, e.msg)
 }
 
+// GetType returns the exception type.
 func (e Exception) GetType() string {
 	return e._type
 }
 
+// GetMsg returns the exception message.
 func (e Exception) GetMsg() string {
 	return e.msg
 }
 
+// IsChildByString is equal to IsChildByString(e.GetType(), parent).
 func (e Exception) IsChildByString(parent string) bool {
 	return IsChildByString(e._type, parent)
 }
 
+// IsChild is equal to IsChildByString(e.GetType(), parent.GetType()).
 func (e Exception) IsChild(parent Exception) bool {
 	return IsChildByString(e._type, parent._type)
 }
 
+// IsSame is equal to IsSame(e.GetType(), other.GetType()).
 func (e Exception) IsSame(other Exception) bool {
 	return IsSame(e._type, other._type)
 }
 
+// GetTypeList returns a list of the exception types.
+//
+// For example, GetTypeList() on the exception type "e1.e2.e3" returns ["e1", "e2", "e3"].
 func (e Exception) GetTypeList() (list []string) {
 	return getListBySep(e._type, SEPARATOR)
 }
 
 func getListBySep(s string, sep string) (list []string) {
-	orig_list := strings.Split(s, sep)
-	for _, t := range orig_list {
+	origList := strings.Split(s, sep)
+	for _, t := range origList {
 		if t = strings.TrimSpace(t); t != "" {
 			list = append(list, t)
 		}
@@ -69,6 +82,9 @@ func getListBySep(s string, sep string) (list []string) {
 	return
 }
 
+// IsChildByString check whether child is sub-exception of parent or not.
+//
+// Return true if yes, or false.
 func IsChildByString(child, parent string) bool {
 	type1 := fixType(child)
 	type2 := fixType(parent)
@@ -82,6 +98,9 @@ func IsChildByString(child, parent string) bool {
 	return strings.Contains(type1, type2)
 }
 
+// IsSame check whether two exception types is the same.
+//
+// Return ture if yes, or false.
 func IsSame(type1, type2 string) bool {
 	type1 = fixType(type1)
 	type2 = fixType(type2)
@@ -92,6 +111,7 @@ func IsSame(type1, type2 string) bool {
 	return false
 }
 
+// IsChildByExc is same as IsChildByString, but the type is Exception, not string.
 func IsChildByExc(child, parent Exception) bool {
 	return IsChildByString(child._type, parent._type)
 }
