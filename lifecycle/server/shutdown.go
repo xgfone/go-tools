9e7c6f942e2414e9efdfb4e4cf0d25457312ffcc
xgfone <xgfone@126.com>
2017-04-04 18:55:47 +0800

    Add the new function IsShutdowned

--- lifecycle/server/shutdown.go
+++ lifecycle/server/shutdown.go
@@ -1,13 +1,27 @@
 package server
 
-var shouldShutdown = make(chan bool, 1)
+import "github.com/xgfone/go-tools/atomics"
+
+var (
+	shutdowned     = atomics.NewBool()
+	shouldShutdown = make(chan bool, 1)
+)
 
 // RunForever runs for ever.
 func RunForever() {
+	if shutdowned.Get() {
+		panic("The server has been shutdowned")
+	}
 	<-shouldShutdown
 }
 
 // Shutdown shutdowns the server gracefully.
 func Shutdown() {
+	shutdowned.SetTrue()
 	shouldShutdown <- true
+}
+
+// IsShutdowned returns whether the server has been shutdowned.
+func IsShutdowned() bool {
+	return shutdowned.Get()
 }
