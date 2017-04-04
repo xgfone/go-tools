0a97c78b765c44cd52f446cf4fa7bbfc491bff40
xgfone <xgfone@126.com>
2017-04-04 11:21:05 +0800

    Add shutdown and forever

---
+++ lifecycle/shutdown.go
@@ -0,0 +1,13 @@
+package lifecycle
+
+var shouldShutdown = make(chan bool, 1)
+
+// RunForever runs for ever.
+func RunForever() {
+	<-shouldShutdown
+}
+
+// Shutdown shutdowns the server gracefully.
+func Shutdown() {
+	shouldShutdown <- true
+}
