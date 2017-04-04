8caa04d8300811809b3a3db757cfb9527ed4662b
xgfone <xgfone@126.com>
2017-04-04 20:30:30 +0800

    Add lifecycle.Manager for shutdown

--- lifecycle/server/shutdown.go
+++ lifecycle/server/shutdown.go
@@ -1,8 +1,12 @@
 package server
 
-import "github.com/xgfone/go-tools/atomics"
+import (
+	"github.com/xgfone/go-tools/atomics"
+	"github.com/xgfone/go-tools/lifecycle"
+)
 
 var (
+	manager        = lifecycle.GetDefaultManager()
 	shutdowned     = atomics.NewBool()
 	shouldShutdown = make(chan bool, 1)
 )
@@ -18,10 +22,17 @@ func RunForever() {
 // Shutdown shutdowns the server gracefully.
 func Shutdown() {
 	shutdowned.SetTrue()
+	manager.Stop()
 	shouldShutdown <- true
 }
 
 // IsShutdowned returns whether the server has been shutdowned.
 func IsShutdowned() bool {
 	return shutdowned.Get()
+}
+
+// RegisterManager replaces the default lifecycle manager.
+// The default manager is the default global manager in the package lifecycle.
+func RegisterManager(m *lifecycle.Manager) {
+	manager = m
 }
