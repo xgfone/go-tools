3890f3ddefd15de8a097e0356da738bea94855d1
xgfone <xgfone@126.com>
2017-10-25 13:05:22 +0800

    Add HTTP error handler.

--- nets/https/http.go
+++ nets/https/http.go
@@ -4,6 +4,8 @@ package https
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 )
 
 // HTTPError stands for a HTTP error.
@@ -32,4 +34,40 @@ func NewHTTPError(flag int, err interface{}) error {
 
 func (e HTTPError) Error() string {
 	return e.Err.Error()
+}
+
+// ErrorLogFunc handles the http error log in ErrorHandler and
+// ErrorHandlerWithStatusCode.
+//
+// Notice: The caller doesn't append the new line, so the function should
+// append the new line.
+var ErrorLogFunc func(format string, args ...interface{})
+
+func init() {
+	ErrorLogFunc = func(format string, args ...interface{}) {
+		fmt.Fprintf(os.Stderr, format+"\n", args...)
+	}
+}
+
+// ErrorHandler handles the error and responds it the client.
+func ErrorHandler(f func(http.ResponseWriter, *http.Request) error) http.HandlerFunc {
+	return ErrorHandlerWithStatusCode(func(w http.ResponseWriter,
+		r *http.Request) (int, error) {
+		return http.StatusInternalServerError, f(w, r)
+	})
+}
+
+// ErrorHandlerWithStatusCode handles the error and responds it the client
+// with the status code.
+func ErrorHandlerWithStatusCode(f func(http.ResponseWriter, *http.Request) (
+	int, error)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if code, err := f(w, r); err != nil {
+			if code == 0 {
+				code = http.StatusInternalServerError
+			}
+			http.Error(w, err.Error(), code)
+			ErrorLogFunc("Handling %q: status=%d, err=%v", r.RequestURI, code, err)
+		}
+	}
 }
