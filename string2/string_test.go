715523d3215c0239994dd3911aaff6d474bc8d94
xgfone <xgfone@126.com>
2017-11-02 13:24:39 +0800

    Add the function to format the string by key-value.

--- string2/string_test.go
+++ string2/string_test.go
@@ -5,6 +5,26 @@ import (
 	"unicode"
 )
 
+func ExampleKvFmt() {
+	tpl := "{key1}, {key2}, {key1}, {key2}"
+	args := map[string]interface{}{"key1": "abc", "key2": "123"}
+	fmt.Println(KvFmt(tpl, args))
+
+	// Output:
+	// abc, 123, abc, 123
+}
+
+func ExampleSetFmtDelimiter() {
+	SetFmtDelimiter("{{", "}}")
+
+	tpl := "{{key1}}, {{key2}}, {{key1}}, {{key2}}"
+	args := map[string]interface{}{"key1": "abc", "key2": "123"}
+	fmt.Println(KvFmt(tpl, args))
+
+	// Output:
+	// abc, 123, abc, 123
+}
+
 func ExampleSplitSpace() {
 	s := "   1   2   3   "
 	ss := SplitSpace(s)
--- string2/strings.go
+++ string2/strings.go
@@ -3,8 +3,40 @@ package string2
 
 import (
 	"bytes"
+	"fmt"
+	"strings"
 	"unicode"
 )
+
+var (
+	leftDelimiter  = "{"
+	rightDelimiter = "}"
+)
+
+// SetFmtDelimiter sets the delimiters which are used by KvFmt.
+//
+// The left delimiter is "{", and the right delimiter is "}".
+func SetFmtDelimiter(left, right string) {
+	if left == "" || right == "" {
+		panic("The arguments cannot be empty")
+	}
+	leftDelimiter = left
+	rightDelimiter = right
+}
+
+// KvFmt formats the string like the key-value method format of str in Python,
+// which the placeholder is appointed by the key name of the values.
+//
+// Notice: the formatter will use %v to convert the value of the key to string.
+// The delimiters are "{" and "}" by default, and you can reset them by the
+// function SetFmtDelimiter.
+func KvFmt(s string, values map[string]interface{}) string {
+	for key, value := range values {
+		key = fmt.Sprintf("%s%s%s", leftDelimiter, key, rightDelimiter)
+		s = strings.Replace(s, key, fmt.Sprintf("%v", value), -1)
+	}
+	return s
+}
 
 // SplitSpace splits the string of s by the whitespace, which is equal to
 // str.split() in Python.
