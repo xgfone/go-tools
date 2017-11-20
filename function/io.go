70d8b87eebedcf5e8f6d3f52eac18d4f1a511d9f
xgfone <xgfone@126.com>
2017-11-20 23:56:36 +0800

    Add the Close wrapper type

---
+++ function/io.go
@@ -0,0 +1,14 @@
+package function
+
+import "io"
+
+// Close implements the interface with the method Close(), which does not return
+// an error.
+type Close struct {
+	Value io.Closer
+}
+
+// Close implements the method Close().
+func (c Close) Close() {
+	c.Value.Close()
+}
