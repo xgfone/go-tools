fa411078c7b3b7f3dc58e4cee1a3963c0c323c7b
xgfone <xgfone@126.com>
2018-12-25 16:16:02 +0800

    fix a bug about ReadN

--- io2/io.go
+++ io2/io.go
@@ -3,9 +3,8 @@ package io2
 
 import (
 	"bufio"
+	"bytes"
 	"io"
-
-	"github.com/xgfone/go-tools/pools"
 )
 
 // ReadLine reads the content in the buffer by line.
@@ -24,28 +23,43 @@ func ReadLine(r *bufio.Reader) (lines [][]byte, err error) {
 // ReadN reads the data from io.Reader until n bytes or no incoming data
 // if n is equal to or less than 0.
 func ReadN(r io.Reader, n int64) (v []byte, err error) {
-	w := pools.DefaultBufferPool.Get()
-	err = ReadNWriter(w, r, n)
-	v = w.Bytes()
-	pools.DefaultBufferPool.Put(w)
-	return v, err
+	buf := bytes.NewBuffer(nil)
+	err = ReadNBuffer(buf, r, n)
+	return buf.Bytes(), err
+}
+
+// ReadNBuffer reads n bytes into buf from r.
+func ReadNBuffer(buf *bytes.Buffer, r io.Reader, n int64) error {
+	if n < 1 {
+		_, err := io.Copy(buf, r)
+		return err
+	}
+
+	if n < 32768 { // 32KB
+		buf.Grow(int(n))
+	} else {
+		buf.Grow(32768)
+	}
+
+	if m, err := io.Copy(buf, io.LimitReader(r, n)); err != nil {
+		return err
+	} else if m < n {
+		return io.EOF
+	}
+	return nil
 }
 
 // ReadNWriter is the same as ReadN, but writes the data to the writer
 // from the reader.
 func ReadNWriter(w io.Writer, r io.Reader, n int64) (err error) {
-	buf := pools.BytesPool2K.Get()
-
 	if n > 0 {
 		var m int64
-		m, err = io.CopyBuffer(w, io.LimitReader(r, n), buf)
+		m, err = io.Copy(w, io.LimitReader(r, n))
 		if m < n && err == nil {
 			err = io.EOF
 		}
 	} else {
-		_, err = io.CopyBuffer(w, r, buf)
+		_, err = io.Copy(w, r)
 	}
-
-	pools.BytesPool2K.Put(buf)
 	return
 }
