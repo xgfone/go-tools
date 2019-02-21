f13fe31f2beb07b192c4bc93f22965d32dfdf7cd
xgfone <xgfone@126.com>
2019-02-21 11:00:07 +0800

    fix a test error

--- option/option_test.go
+++ option/option_test.go
@@ -1,7 +1,6 @@
 package option
 
 import (
-	"database/sql"
 	"testing"
 )
 
@@ -22,14 +21,5 @@ func TestNamedOption(t *testing.T) {
 
 	if NamedSome("", 123).IsNone() || NamedSome("", 123).SomeOr(456).(int) != 123 {
 		t.Fail()
-	}
-
-	db, err := sql.Open("mysql", `root:singularity0618@tcp(10.241.230.112:3306)/snda_cloud_accounts`)
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
-	if err = db.Ping(); err != nil {
-		t.Error(err)
 	}
 }
