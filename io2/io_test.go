7b10f7b9cf7f2eab079e774bfad2374fd5720043
xgfone <xgfone@126.com>
2019-03-07 11:07:19 +0800

    add io test

--- io2/io_test.go
+++ io2/io_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"testing"
 )
 
 func ExampleReadN() {
@@ -33,4 +34,28 @@ func ExampleReadN() {
 	// OK
 	// OK
 	// OK
+}
+
+func TestReadNWriter(t *testing.T) {
+	writer := bytes.NewBuffer(nil)
+	reader := bytes.NewBufferString("test")
+	if ReadNWriter(writer, reader, 4) != nil || writer.String() != "test" {
+		t.Errorf("writer: %s", writer.String())
+	}
+
+	writer = bytes.NewBuffer(nil)
+	reader = bytes.NewBufferString("test")
+	if ReadNWriter(writer, reader, 2) != nil || writer.String() != "te" {
+		t.Errorf("writer: %s", writer.String())
+	} else if ReadNWriter(writer, reader, 2) != nil || writer.String() != "test" {
+		t.Errorf("writer: %s", writer.String())
+	}
+
+	writer = bytes.NewBuffer(nil)
+	reader = bytes.NewBufferString("test")
+	if err := ReadNWriter(writer, reader, 5); err == nil {
+		t.Error("non-nil")
+	} else if err != io.EOF {
+		t.Error(err)
+	}
 }
