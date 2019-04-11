d5b39320ae1fc5814e6bff2786ca20031e015fa9
xgfone <xgfone@126.com>
2019-04-11 15:18:36 +0800

    add the TCP server

--- net2/tcp.go
+++ net2/tcp.go
@@ -17,6 +17,8 @@ package net2
 import (
 	"fmt"
 	"net"
+	"sync"
+	"sync/atomic"
 )
 
 // TCPServerForever starts a TCP server. If starting successfully, never return.
@@ -43,6 +45,75 @@ func TCPServerForever(addr string, handler func(*net.TCPConn)) error {
 
 	// Never execute forever.
 	// return nil
+}
+
+// TCPServer is used to manage a TCP server.
+type TCPServer struct {
+	Listener *net.TCPListener
+	Handler  func(conn *net.TCPConn, isStopped func() bool)
+
+	waits  sync.WaitGroup
+	closed int32
+}
+
+// NewTCPServer returns a new TCPServer.
+func NewTCPServer(ln *net.TCPListener, handler func(conn *net.TCPConn, isStopped func() bool)) *TCPServer {
+	return &TCPServer{Listener: ln, Handler: handler}
+}
+
+// NewTCPServerFromAddr returns a new TCPServer listening on addr.
+func NewTCPServerFromAddr(addr string, handler func(conn *net.TCPConn, isStopped func() bool)) (*TCPServer, error) {
+	_addr, err := net.ResolveTCPAddr("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	ln, err := net.ListenTCP("tcp", _addr)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewTCPServer(ln, handler), nil
+}
+
+// Start starts the TCP server.
+func (s *TCPServer) Start() {
+	s.waits.Add(1)
+	defer s.waits.Done()
+
+	for {
+		conn, err := s.Listener.AcceptTCP()
+		if err != nil {
+			return
+		}
+
+		s.waits.Add(1)
+		go func() {
+			defer func() {
+				conn.Close()
+				s.waits.Done()
+			}()
+
+			s.Handler(conn, s.IsStopped)
+		}()
+	}
+}
+
+// Stop stops the TCP server.
+func (s *TCPServer) Stop() {
+	if atomic.CompareAndSwapInt32(&s.closed, 0, 1) {
+		s.Listener.Close()
+	}
+}
+
+// Wait waits until all the connections are closed and exit.
+func (s *TCPServer) Wait() {
+	s.waits.Wait()
+}
+
+// IsStopped reports whether the TCP server is stopped
+func (s *TCPServer) IsStopped() bool {
+	return atomic.LoadInt32(&s.closed) == 1
 }
 
 // DialTCP dials a TCP connection to host:port.
