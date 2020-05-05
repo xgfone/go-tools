b62825b9e3b0c2479ce9259fc0969949a8de148e
xgfone <xgfone@126.com>
2020-05-05 18:38:28 +0800

    update the bytes and buffer pools

--- pools/buf.go
+++ pools/buf.go
@@ -20,40 +20,47 @@ import (
 )
 
 var (
-	// DefaultBufferPool is the default global buffer pool.
-	DefaultBufferPool = NewBufferPool()
+	// BufferPool1k is the buffer pool with 1K initialized capacity.
+	BufferPool1k = NewBufferPool(1024)
 
-	// BytesPool1K the bytes buffer with 1K buffer.
+	// BufferPool2k is the buffer pool with 2K initialized capacity.
+	BufferPool2k = NewBufferPool(2048)
+
+	// BufferPool4k is the buffer pool with 4K initialized capacity.
+	BufferPool4k = NewBufferPool(4096)
+
+	// BufferPool8k is the buffer pool with 8K initialized capacity.
+	BufferPool8k = NewBufferPool(8192)
+
+	// BytesPool1K the bytes pool with 1K buffer.
 	BytesPool1K = NewBytesPool(1024)
 
-	// BytesPool2K the bytes buffer with 2K buffer.
+	// BytesPool2K the bytes pool with 2K buffer.
 	BytesPool2K = NewBytesPool(2048)
 
-	// BytesPool4K the bytes buffer with 4K buffer.
+	// BytesPool4K the bytes pool with 4K buffer.
 	BytesPool4K = NewBytesPool(4096)
 
-	// BytesPool8K the bytes buffer with 8K buffer.
+	// BytesPool8K the bytes pool with 8K buffer.
 	BytesPool8K = NewBytesPool(8192)
 )
 
 // BytesPool is the []byte wrapper of sync.Pool
 type BytesPool struct {
 	size int
-	pool *sync.Pool
+	pool sync.Pool
 }
 
 // NewBytesPool returns a new []byte pool.
 //
 // size is the size of the []byte.
-func NewBytesPool(size int) BytesPool {
-	pool := &sync.Pool{New: func() interface{} {
-		return make([]byte, size)
-	}}
-	return BytesPool{pool: pool, size: size}
+func NewBytesPool(size int) *BytesPool {
+	newf := func() interface{} { return make([]byte, size) }
+	return &BytesPool{pool: sync.Pool{New: newf}, size: size}
 }
 
 // Get returns a []byte.
-func (p BytesPool) Get() []byte {
+func (p *BytesPool) Get() []byte {
 	x := p.pool.Get()
 	if x == nil {
 		return make([]byte, p.size)
@@ -62,7 +69,7 @@ func (p BytesPool) Get() []byte {
 }
 
 // Put places a []byte to the pool.
-func (p BytesPool) Put(b []byte) {
+func (p *BytesPool) Put(b []byte) {
 	if b != nil {
 		p.pool.Put(b)
 	}
@@ -70,7 +77,7 @@ func (p BytesPool) Put(b []byte) {
 
 // BufferPool is the bytes.Buffer wrapper of sync.Pool
 type BufferPool struct {
-	pool *sync.Pool
+	pool sync.Pool
 	size int
 }
 
@@ -81,23 +88,13 @@ func makeBuffer(size int) (b *bytes.Buffer) {
 }
 
 // NewBufferPool returns a new bytes.Buffer pool.
-//
-// bufSize is the initializing size of the buffer. If the size is equal to
-// or less than 0, it will be ignored, and use the default size, 1024.
-func NewBufferPool(bufSize ...int) BufferPool {
-	size := 1024
-	if len(bufSize) > 0 && bufSize[0] > 0 {
-		size = bufSize[0]
-	}
-
-	pool := &sync.Pool{New: func() interface{} {
-		return makeBuffer(size)
-	}}
-	return BufferPool{pool: pool, size: size}
+func NewBufferPool(size int) *BufferPool {
+	newf := func() interface{} { return makeBuffer(size) }
+	return &BufferPool{pool: sync.Pool{New: newf}, size: size}
 }
 
 // Get returns a bytes.Buffer.
-func (p BufferPool) Get() *bytes.Buffer {
+func (p *BufferPool) Get() *bytes.Buffer {
 	x := p.pool.Get()
 	if x == nil {
 		return makeBuffer(p.size)
@@ -106,7 +103,7 @@ func (p BufferPool) Get() *bytes.Buffer {
 }
 
 // Put places a bytes.Buffer to the pool.
-func (p BufferPool) Put(b *bytes.Buffer) {
+func (p *BufferPool) Put(b *bytes.Buffer) {
 	if b != nil {
 		b.Reset()
 		p.pool.Put(b)
